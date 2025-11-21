96a80f5efa664658f4475f122bffd36d1f07b63f
Dmitrijs Kaminskis <dke@betitall.com>
2025-11-21 14:45:33 +0200

    Stop tracking game folder

--- game/scripts/vscripts/lib/Empty.lua
+++
@@ -1,6 +0,0 @@
-local ____lualib = require("lualib_bundle")
-local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["5"] = 1});
-local ____exports = {}
-____exports.empty = function() return nil end
-return ____exports
